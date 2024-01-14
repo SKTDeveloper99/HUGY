1b6934bb231dbf6148374b3d1c8f28e46327527d
BerniceZhang333 <153250339+BerniceZhang333@users.noreply.github.com>
2024-01-13 23:28:17 -0500

    Update meditation_page.dart

--- lib/screens/activities/meditation_page.dart
+++ lib/screens/activities/meditation_page.dart
@@ -14,27 +14,99 @@ class MeditationPage extends StatefulWidget {
 class _MeditationPageState extends State<MeditationPage> {
   Timer? _timer;
 
-  int _seconds = 0;
-  final int max_seconds = 60 * 60 * 24;
+  String twoDigits(int n) => n.toString().padLeft(2, "0");
 
-  String formatTime(int time) {
-    int hours = time ~/ 3600;
-    int minutes = (time - hours * 3600) ~/ 60;
-    int seconds = time - hours * 3600 - minutes * 60;
-    return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  int max_seconds = 60 * 5;
+  Duration duration = Duration();
+
+  int _seconds = 0;
+  int _minutes = 0;
+  int _hours = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    duration = Duration(seconds: max_seconds);
+    _seconds = duration.inSeconds;
+    _minutes = duration.inMinutes;
+    _hours = duration.inHours;
   }
 
-  void startTimer() {}
-  void stopTimer() {}
-  void resetTimer() {}
+  @override
+  void dispose() {
+    stopTimer();
+    super.dispose();
+  }
 
-  Widget buildClock() {
+  void updateTimer() {
+    setState(() {
+      duration = Duration(seconds: max_seconds);
+      max_seconds--;
+      _seconds = duration.inSeconds;
+      _minutes = duration.inMinutes;
+      _hours = duration.inHours;
+    });
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      updateTimer();
+    });
+  }
+
+  void stopTimer() {
+    // cancel the timer
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+  }
+
+  // set the timer to zero seconds
+  void resetTimer() {
+    setState(() {
+      max_seconds = 60 * 5;
+      duration = Duration(seconds: max_seconds);
+      _seconds = duration.inSeconds;
+      _minutes = duration.inMinutes;
+      _hours = duration.inHours;
+    });
+  }
+
+  Widget buildTimeSlot(int time) {
     return Container(
-      child: Text(
-        '${formatTime(_seconds)}',
-        style: TextStyle(fontSize: 60),
+      width: 50,
+      height: 50,
+      child: Card(
+        child: Center(
+          child: Text(
+            twoDigits(time),
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
       ),
     );
+  }
+
+  Widget buildClock() {
+    return SizedBox(
+        width: 200,
+        height: 200,
+        child: Stack(fit: StackFit.expand, children: [
+          CircularProgressIndicator(
+            value: duration.inSeconds / max_seconds,
+            strokeWidth: 10,
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildTimeSlot(_hours),
+                buildTimeSlot(_minutes),
+                buildTimeSlot(_seconds),
+              ],
+            ),
+          )
+        ]));
   }
 
   @override
@@ -48,8 +120,24 @@ class _MeditationPageState extends State<MeditationPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   buildClock(),
-                  ElevatedButton(
-                      onPressed: () {}, child: Text("Start Meditation"))
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() => startTimer());
+                          },
+                          child: Text("Start")),
+                      ElevatedButton(
+                        onPressed: () => stopTimer(),
+                        child: Text("Stop"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => resetTimer(),
+                        child: Text("Reset"),
+                      ),
+                    ],
+                  ),
                 ],
               )),
             )));
